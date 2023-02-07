d2aa5c90545506077eda453db9af8250ec65e67d
godtyze <omgfail301@gmail.com>
2023-02-07 23:11:14 +0600

    feat: add error handle middleware

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const PORT = process.env.PORT || 7000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api', router)
+app.use('/api', router);
+
+app.use(errorHandler);
 
 const start = async () => {
   try {
